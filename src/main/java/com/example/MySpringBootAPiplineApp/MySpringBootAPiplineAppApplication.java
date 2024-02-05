11a823abb818302cbb9791fcc9e4318106553b38
Shima N <shiman@Shimas-MBP.socal.rr.com>
2024-02-04 20:40:28 -0800

    pushing all code

---
+++ src/main/java/com/example/MySpringBootAPiplineApp/MySpringBootAPiplineAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MySpringBootAPiplineApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootAPiplineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootAPiplineAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MySpringBootAPiplineApp/MySpringBootAPiplineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MySpringBootAPiplineApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootAPiplineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
